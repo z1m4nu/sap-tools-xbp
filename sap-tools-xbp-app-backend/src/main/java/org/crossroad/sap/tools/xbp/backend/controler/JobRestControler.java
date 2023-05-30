45dc3c7b07760faa986d23570ad11e30504b06ef
Manu <e.soden@gmail.com>
2023-05-30 23:35:37 +0200

    Update

---
+++ sap-tools-xbp-app-backend/src/main/java/org/crossroad/sap/tools/xbp/backend/controler/JobRestControler.java
@@ -0,0 +1,10 @@
+package org.crossroad.sap.tools.xbp.backend.controler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/xbp/job/v1")
+public class JobRestControler {
+
+}
